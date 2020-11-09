8247019998fe4054ec72b4b73642f7c97db67252
Colin Davenport <colindaven@hotmail.com>
2020-11-09 20:38:07 +0000

    Updates and now working

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@ use csv::Reader;
 //use csv::StringRecord;
 use std::collections::HashMap; 
 use std::process;
-use std::str;
+//use std::str;
+
+
+// Check a SampleSheet CSV file for multiple common lab errors. 
+// Does not check duplicate indices or similar.
 
 fn check_csv() -> Result<(), Box<dyn Error>> {
     let mut sample_id_count : u16 = 0;
@@ -21,9 +25,16 @@ fn check_csv() -> Result<(), Box<dyn Error>> {
         for field in record.iter() {
 
             //Exit on Umlaut 
-            assert!(field.contains("ö"));
-            //assert!(field.contains "ö" || field != "Ö" || field != "Ü"|| field != "ü"|| field != "ä"|| field != "Ä");
+            if field.contains("ö") || field.contains("Ö") || field.contains("Ü") || field.contains("ü") || field.contains("ä") || field.contains("Ä") {
+                println!("ERROR: Umlaut found, exiting. Field: {}", field);
+                break;
+            }
 
+        //Exit on semicolon
+        if field.contains(";") {
+            println!("ERROR: Semicolon ; illegal found, exiting. Field: {}", field);
+            break;
+        }
 
             // Check sample ID counts
             //assert!(field == "Sample_ID");
@@ -47,23 +58,27 @@ fn check_csv() -> Result<(), Box<dyn Error>> {
 //fn check_string() -> Result<(), Box<dyn Error>> {
 
 
+// 
+//  Main method    
+
 fn main() {
 
-    println!("Welcome to validate Samplesheet by Colin Davenport");
-    println!("Warning: does not catch Umlaeute öäü at present");
+    let version = "0.1";
+    println!("INFO: Welcome to Validate Samplesheet v{} by Colin Davenport", version);
+    println!("INFO: Prints error on using ; anywhere instead of comma");
+    println!("INFO: Prints error on finding German Umlaut");
+    println!("INFO: Prints error if number of fields are not correct");
+    println!("INFO: Prints error Sample_ID is present twice, should be Sample_ID, Sample_Name");
+    //println!("Warning: does not catch xxxxx at present");
 
     let map: HashMap<u32, u32> = HashMap::new(); 
     
 
     if let Err(err) = check_csv() {
-        println!("error running check_csv: {}", err);
+        println!("ERROR running check_csv: {}", err);
         println!("You can probably solve this by opening in Libreoffice or Excel and deleting the unused whitespace columns, then saving as CSV again.");
         process::exit(1);
     }
 
-    //let record : StringRecord = check_csv();
-    //for field in record.iter() {
-    //    assert!(field == "a" || field == "b" || field == "c");
-    //}
 
 }
