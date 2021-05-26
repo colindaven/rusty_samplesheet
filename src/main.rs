59a5b4792a10332ede011821fe74d87e2e0b1192
Colin Davenport <davenport.colin@mh-hannover.de>
2021-05-26 19:42:49 +0200

    Add and test arg parsing, v0.21

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 extern crate csv;
+extern crate argparse;
 
+use argparse::{ArgumentParser, StoreTrue, Store};
 use std::error::Error;
 use csv::Reader;
 //use csv::StringRecord;
-use std::collections::HashMap; 
+//use std::collections::HashMap; 
 use std::process;
 //use std::str;
 
@@ -11,11 +13,12 @@ use std::process;
 // Check a SampleSheet CSV file for multiple common lab errors. 
 // Does not check duplicate indices or similar.
 
-fn check_csv() -> Result<(), Box<dyn Error>> {
+fn check_csv(csv_file_string: String) -> Result<(), Box<dyn Error>> {
     let mut sample_id_count : u16 = 0;
 
     // Setup file and read
-    let mut rdr = Reader::from_path("SampleSheet_2020_032049.csv")?;
+    //let mut rdr = Reader::from_path("SampleSheet_2020_032049.csv")?;
+    let mut rdr = Reader::from_path(csv_file_string)?;
     // Iterate through results.
     for result in rdr.records() {
         let record = result?;
@@ -26,13 +29,20 @@ fn check_csv() -> Result<(), Box<dyn Error>> {
 
             //Exit on Umlaut 
             if field.contains("ö") || field.contains("Ö") || field.contains("Ü") || field.contains("ü") || field.contains("ä") || field.contains("Ä") {
+                println!("");
                 println!("ERROR: Umlaut found, exiting. Field: {}", field);
+                println!("Line containing error: {:?}", record);
+                //let error_string = "ERROR: Umlaut found, exiting. Field: {}", field;
+                //let error_record = record.clone();
+                //print_errors(error_string, error_record);
                 break;
             }
 
         //Exit on semicolon
         if field.contains(";") {
-            println!("ERROR: Semicolon ; illegal found, exiting. Field: {}", field);
+            println!("");
+            println!("ERROR: Semicolon ; illegal found, Only commas , should be used! Exiting. Field: {}", field);
+            println!("Line containing error: {:?}", record);
             break;
         }
 
@@ -41,7 +51,9 @@ fn check_csv() -> Result<(), Box<dyn Error>> {
             if field == "Sample_ID" {
                 sample_id_count = sample_id_count +  1;
                 if sample_id_count > 1 {
+                    println!("");
                     println!("ERROR: Sample_ID present more than once: {}, {}, {:?}", sample_id_count, field, record);
+                    println!("Line containing error: {:?}", record);
                 }
             }
 
@@ -54,8 +66,23 @@ fn check_csv() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+fn print_errors(bad_string: String, record: String){
+    println!("");
+    println!("Line containing error: {:?}", record);
+    println!("");
 
-//fn check_string() -> Result<(), Box<dyn Error>> {
+} 
+
+
+fn report_checks_as_info()  {
+    println!("INFO: Prints error on using ; anywhere instead of comma");
+    println!("INFO: Prints error on finding German Umlaut");
+    println!("INFO: Prints error if number of fields are not correct");
+    println!("INFO: Prints error Sample_ID is present twice, should be Sample_ID, Sample_Name");
+    println!("INFO: Prints error if more than three speech marks on line");
+    println!("INFO: Prints error if Sample_ID is present twice, should be Sample_ID, Sample_Name");
+}
+
 
 
 // 
@@ -63,18 +90,38 @@ fn check_csv() -> Result<(), Box<dyn Error>> {
 
 fn main() {
 
-    let version = "0.1";
-    println!("INFO: Welcome to Rusty Samplesheet v{} by Colin Davenport", version);
-    println!("INFO: Prints error on using ; anywhere instead of comma");
-    println!("INFO: Prints error on finding German Umlaut");
-    println!("INFO: Prints error if number of fields are not correct");
-    println!("INFO: Prints error Sample_ID is present twice, should be Sample_ID, Sample_Name");
-    //println!("Warning: does not catch xxxxx at present");
+    let version = "0.21";
+    //0.21 - add args parsing
+    //0.20 - cross compile for Windows
+    //0.10 - first version
 
-    let map: HashMap<u32, u32> = HashMap::new(); 
+    println!("INFO: Welcome to Rusty Samplesheet version {} by Colin Davenport", version);
+    //report_checks_as_info();
+
+    //let _map: HashMap<u32, u32> = HashMap::new(); 
     
+    ////////////////
+    // Parse input arguments
 
-    if let Err(err) = check_csv() {
+    let mut input_file = "SampleSheet.csv".to_string();
+    let mut name = "horse".to_string();
+    {  // this block limits scope of borrows by parser.refer() method
+        let mut parser = ArgumentParser::new();
+        parser.refer(&mut name)
+            .add_option(&["--name"], Store,
+            "Name for the greeting");
+        parser.refer(&mut input_file);    
+        //parser.refer(&mut input_file)
+        //    .add_option(&["-f", "--input_file"], Store,
+        //                "Input file CSV");
+        parser.parse_args_or_exit();
+    } 
+    let mut input_csv: String = str::to_string(&name);
+
+    ////////////////
+    // Parse and check the CSV
+
+    if let Err(err) = check_csv(input_csv) {
         println!("ERROR running check_csv: {}", err);
         println!("You can probably solve this by opening in Libreoffice or Excel and deleting the unused whitespace columns, then saving as CSV again.");
         process::exit(1);
