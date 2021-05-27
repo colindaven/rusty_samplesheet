564b28c5da917860990e482e2319944c737ef34d
Dr. Colin Davenport <davenport.colin@mh-hannover.de>
2021-05-27 10:26:56 +0200

    Rebuild

--- copy_built_release.sh
+++ copy_built_release.sh
@@ -1,3 +1,12 @@
+echo "Build Linux and Windows versions and copy the binaries to this directory"
+echo "Usage: hpc03 only. bash copy_built_release.sh"
+
+cargo rustc --target=i686-pc-windows-gnu --release -- -C link-args=-mwindows
+#You can build your crate easily with
+#cargo build --target x86_64-pc-windows-gnu
+## Compile for Linux
+cargo build --target release
+
 cp target/x86_64-pc-windows-gnu/release/*.exe .
 cp target/release/validate_samplesheet .
 
