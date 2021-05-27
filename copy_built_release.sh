bfa455ac63b8899dcffecd52daf7cf83b01f0809
Dr. Colin Davenport <davenport.colin@mh-hannover.de>
2021-05-27 10:52:59 +0200

    build 0.23 windows

--- copy_built_release.sh
+++ copy_built_release.sh
@@ -1,9 +1,20 @@
 echo "Build Linux and Windows versions and copy the binaries to this directory"
 echo "Usage: hpc03 only. bash copy_built_release.sh"
 
+
+
+echo "####### Building on Windows ##########" 
+# now setup rust
+rustup target add x86_64-pc-windows-gnu
+rustup toolchain install stable-x86_64-pc-windows-gnu
+
 #cargo rustc --target=i686-pc-windows-gnu --release -- -C link-args=-mwindows
 #You can build your crate easily with
 cargo build --target x86_64-pc-windows-gnu
+
+
+echo "####### Building on Linux ##########" 
+
 ## Compile for Linux
 cargo build --target release
 
